011e0cd9b3c51ead93c294c39f20ede411241777
Luckey <903804013@qq.com>
2016-08-17 15:58:25 +0800

    删除了一个垃圾文件

--- app/src/main/java/com/qf/luckey/adapter/lll.java
+++
@@ -1,7 +0,0 @@
-package com.qf.luckey.adapter;
-
-/**
- * Created by Luckey on 2016/8/17.
- */
-public class lll {
-}
--- app/src/main/java/com/qf/luckey/adapter/vvv.java
+++ app/src/main/java/com/qf/luckey/adapter/vvv.java
@@ -3,5 +3,4 @@ package com.qf.luckey.adapter;
 /**
  * Created by Luckey on 2016/8/17.
  */
-public class vvv {
-}
+public class vvv {}
